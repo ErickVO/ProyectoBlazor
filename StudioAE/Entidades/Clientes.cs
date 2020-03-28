bc7eedbee8cee803cd306171da8975ca5ad12199
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-03-27 22:30:39 -0400

    Entidades

---
+++ StudioAE/Entidades/Clientes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StudioAE.Entidades
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public int UsuarioId { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public String Cedula { get; set; }
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+        public String Celular { get; set; }
+        public String Sexo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        public Clientes()
+        {
+            ClienteId = 0;
+            UsuarioId = 0;
+            Nombre = string.Empty;
+            Apellido = string.Empty;
+            Cedula = string.Empty;
+            Direccion = string.Empty;
+            Telefono = string.Empty;
+            Celular = string.Empty;
+            Sexo = string.Empty;
+            FechaNacimiento = DateTime.Now;
+        }
+    }
+}
