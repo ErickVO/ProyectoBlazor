e794504a6a482fa2d5a63c3ae51535c43fd9c079
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-03-27 22:10:23 -0400

    Entidad Articulos

---
+++ StudioAE/Entidades/Articulos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Articulos
+{
+    [Key]
+    public int ArticulosId { get; set; }
+    public int UsuariosId { get; set; }
+    public string NombreArticulos { get; set; }
+    public string Categorias { get; set; }
+    public int Stock { get; set; }
+    public decimal Precio { get; set; }
+
+    [ForeignKey("ArticulosId")]
+    public List<ComprasDetalle> ComprasDetalle { get; set; }
+
+    [ForeignKey("ArticulosId")]
+    public List<VentasDetalle> VentasDetalle { get; set; }
+
+    public Articulos()
+	{
+
+
+
+	}
+}
