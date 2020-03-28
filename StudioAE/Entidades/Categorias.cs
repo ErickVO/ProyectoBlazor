a4829ce1a24bc71e648a53181922d1fc2831ed9e
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-03-27 22:28:18 -0400

    Entidad Categorias

---
+++ StudioAE/Entidades/Categorias.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Categorias
+{
+    [Key]
+    public int CategoriasId { get; set; }
+    public int UsuarioId { get; set; }
+    public string NombreCategorias { get; set; }
+
+    public Categorias()
+	{
+        CategoriasId = 0;
+        UsuarioId = 0;
+        NombreCategorias = string.Empty;
+	}
+}
