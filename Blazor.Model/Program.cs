eeb14c787d6607d9a740959e177462654b839783
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-05-20 20:46:35 -0400

    Proyecto Final Blazor

---
+++ Blazor.Model/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blazor.Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
